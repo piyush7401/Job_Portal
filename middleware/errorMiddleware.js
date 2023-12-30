eb3d44d5686a9cb50f0df47278519ffc72489049
Piyush Varshney <piyush07varshney@gmail.com>
2023-12-30 12:02:12 +0530

    error middleware done

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,23 +1,24 @@
 //error middleware || NEXT function
 const errorMiddleware = (err, req, res, next) => {
     console.log(err);
+
     const defaultErrors = {
       statusCode: 500,
       message: err,
     };
   
     // missing filed error
-    if (err.name === "ValidationError") {
+    if (err.name === "ValidationError") {              //yeh err.name yeh sab err ki property hoti hai
       defaultErrors.statusCode = 400;
-      defaultErrors.message = Object.values(err.errors)
+      defaultErrors.message = Object.values(err.errors)  //sare errors defaulterrors mai ayenge
         .map((item) => item.message)
         .join(",");
     }
   
     // duplicate error
-    if (err.code && err.code === 11000) {
+    if (err.code && err.code === 11000) {        
       defaultErrors.statusCode = 400;
-      defaultErrors.message = `${Object.keys(
+      defaultErrors.message = `${Object.keys(                         
         err.keyValue
       )} field has to be unique`;
     }
