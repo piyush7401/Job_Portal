e3eec1b03b0921852f9e8c5914975c8db68d1ce7
Piyush Varshney <piyush07varshney@gmail.com>
2024-01-11 23:31:54 +0530

    Login form done

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
+import InputForm from '../components/share/input';
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    try {
+      console.log( email, password );
+    } catch {
+      console.log(e.error);
+    }
+  };
+
+  const handlePasswordchange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleEmailchange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="form-container">
+        <form className="card p-5" onSubmit={handlesubmit}>
+          <div className="mb-1">
+            <h1>Registration is Required!!</h1>
+          </div>
+          <InputForm
+            htmlFor="email"
+            labelText="Email"
+            type="email"
+            name="email"
+            value={email}
+            handleChange={handleEmailchange}
+          ></InputForm>
+          <InputForm
+            htmlFor="password"
+            labelText="Password"
+            type="password"
+            name="password"
+            value={password}
+            handleChange={handlePasswordchange}
+          ></InputForm>
+          <div className="d-flex justify-content-between">
+            <p>
+              Not Registered!! <Link to="/register">Login</Link>
+            </p>
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
